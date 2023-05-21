34b40bb68cfece0baab2ad8deaf8adce42c3502c
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-05-21 19:38:23 -0300

    feat: add routes in ong

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { addressRoutes } from './address.routes';
+import { ongRoutes } from './ong.routes';
 import { userRoutes } from './user.routes';
 
 const router = Router();
 
 router.use('/user', userRoutes);
 router.use('/address', addressRoutes);
+router.use('/ong', ongRoutes);
 
 export { router };
---
+++ src/shared/infra/http/routes/ong.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+
+import { CreateOngController } from '@modules/ong/useCases/create/CreateOngController';
+import { DeleteOngController } from '@modules/ong/useCases/delete/DeleteOngController';
+import { FindByIdOngController } from '@modules/ong/useCases/findById/FindByIdOngController';
+import { FindByNameOngController } from '@modules/ong/useCases/findByName/FindByNameOngController';
+import { UpdateOngController } from '@modules/ong/useCases/update/UpdateOngController';
+
+import { isAuthenticate } from '../middlewares/isAuthenticate';
+
+const ongRoutes = Router();
+
+const findOngByIdController = new FindByIdOngController();
+const findByNameOngController = new FindByNameOngController();
+const createOngController = new CreateOngController();
+const updateOngController = new UpdateOngController();
+const deleteOngController = new DeleteOngController();
+
+ongRoutes.get('/:id', isAuthenticate, findOngByIdController.handle);
+
+ongRoutes.get('/name/:name', isAuthenticate, findByNameOngController.handle);
+
+ongRoutes.post('/', isAuthenticate, createOngController.handle);
+
+ongRoutes.put('/:id', isAuthenticate, updateOngController.handle);
+
+ongRoutes.delete('/:id', isAuthenticate, deleteOngController.handle);
+
+export { ongRoutes };
