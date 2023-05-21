ecd625e1f2f8388600c3ce7da618bed97344cb36
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-05-21 15:10:03 -0300

    feat: add routes address

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { addressRoutes } from './address.routes';
 import { userRoutes } from './user.routes';
 
 const router = Router();
 
 router.use('/user', userRoutes);
+router.use('/address', addressRoutes);
 
 export { router };
