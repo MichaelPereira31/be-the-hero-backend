0488dc2f744597faafd93fbc440c30a902044cb4
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-06-04 23:35:51 -0300

    chore: adicionando base de arquivos

---
+++ src/modules/vacances/dtos/ICreateVacanceDTO.ts
@@ -0,0 +1,9 @@
+export type ICreateVacanceDTO = {
+  title: string;
+  description: string;
+  goal: string;
+  role: string;
+  numberOfPeople: Float32Array;
+  ownerId: string;
+  voluntary: string;
+};
