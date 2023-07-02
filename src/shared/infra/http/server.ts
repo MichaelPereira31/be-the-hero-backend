d395d39cfc7fafe062104fbb7e07e646f4daf431
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-07-02 14:13:31 -0300

    refactor: refactoring

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -14,10 +14,7 @@ dotenv.config();
 
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://be-the-hero-frontend-six.vercel.app',
-  );
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
