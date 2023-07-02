055630cca21192391a7c8ee0649ac8db697c305f
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-07-02 13:06:40 -0300

    refactor: deploy

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,6 +11,15 @@ import responseFormatter from './middlewares/responseFormatter';
 import { router } from './routes';
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://be-the-hero-frontend-six.vercel.app',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(responseFormatter);
