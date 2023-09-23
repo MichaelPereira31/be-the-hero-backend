7e3db2fbdea18e2c54999ddd19ab6e50894d2952
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-09-23 16:45:22 -0300

    refactor: remove find user by type

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -4,7 +4,6 @@ import { AuthenticateController } from '@modules/user/useCases/authenticate/Auth
 import { CreateUserController } from '@modules/user/useCases/create/CreateUserController';
 import { DeleteUserController } from '@modules/user/useCases/delete/DeleteUserController';
 import { FindUserByIdController } from '@modules/user/useCases/findUserById/FindUserByIdController';
-import { FindUserByTypeController } from '@modules/user/useCases/findUserByType/FindUserByTypeController';
 import { UpdateUserController } from '@modules/user/useCases/update/UpdateUserController';
 
 import { isAuthenticate } from '../middlewares/isAuthenticate';
@@ -12,19 +11,12 @@ import { isAuthenticate } from '../middlewares/isAuthenticate';
 const userRoutes = Router();
 
 const findUserByIdController = new FindUserByIdController();
-const findUserByTypeController = new FindUserByTypeController();
 const authenticateUserController = new AuthenticateController();
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserController();
 const deleteUserController = new DeleteUserController();
 
 userRoutes.get('/', isAuthenticate, findUserByIdController.handle);
-
-userRoutes.get(
-  '/type/:userType',
-  isAuthenticate,
-  findUserByTypeController.handle,
-);
 
 userRoutes.post('/authenticate', authenticateUserController.handle);
 
