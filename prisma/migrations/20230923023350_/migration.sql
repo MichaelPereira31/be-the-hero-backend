38fc5bad63cfeabb12090a169c948bd93f4b4dab
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-09-22 23:39:12 -0300

    refactor: refactoring database

---
+++ prisma/migrations/20230923023350_/migration.sql
@@ -0,0 +1,100 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('active', 'inactive');
+
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('ong', 'voluntary');
+
+-- CreateEnum
+CREATE TYPE "EventStatus" AS ENUM ('accepted', 'waiting', 'canceled', 'expired', 'concluded');
+
+-- CreateTable
+CREATE TABLE "address" (
+    "id" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "complement" TEXT NOT NULL,
+    "reference" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ong" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "objective" TEXT NOT NULL,
+    "main_phone" TEXT NOT NULL,
+    "secondary_phone" TEXT NOT NULL,
+    "main_email" TEXT NOT NULL,
+    "secondary_email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "ong_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "avatar" TEXT,
+    "name" TEXT,
+    "last_name" TEXT,
+    "email" TEXT,
+    "password" TEXT,
+    "status" "UserStatus" DEFAULT 'active',
+    "type" "UserType",
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "addressId" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "ongId" TEXT,
+
+    CONSTRAINT "event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "voluntary" (
+    "id" TEXT NOT NULL,
+    "status" "EventStatus" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+
+    CONSTRAINT "voluntary_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "ong" ADD CONSTRAINT "ong_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "address"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "event" ADD CONSTRAINT "event_ongId_fkey" FOREIGN KEY ("ongId") REFERENCES "ong"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "voluntary" ADD CONSTRAINT "voluntary_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "voluntary" ADD CONSTRAINT "voluntary_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
