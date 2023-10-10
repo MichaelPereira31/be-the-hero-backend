54c2ed49a9e338035149e48cdc88e5978b0a641c
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-10-09 23:03:48 -0300

    feat: add route in event

---
+++ src/shared/infra/http/routes/event.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { CreateEventController } from '@modules/event/useCases/create/CreateEventController';
+import { DeleteEventController } from '@modules/event/useCases/delete/DeleteEventController';
+import { FindByIdEventController } from '@modules/event/useCases/findById/FindByIdEventController';
+import { UpdateEventController } from '@modules/event/useCases/update/UpdateEventController';
+
+import { isAuthenticate } from '../middlewares/isAuthenticate';
+
+const eventRoutes = Router();
+
+const findEventByIdController = new FindByIdEventController();
+const createEventController = new CreateEventController();
+const updateEventController = new UpdateEventController();
+const deleteEventController = new DeleteEventController();
+
+eventRoutes.get('/:id', isAuthenticate, findEventByIdController.handle);
+
+eventRoutes.post('/', isAuthenticate, createEventController.handle);
+
+eventRoutes.put('/:id', isAuthenticate, updateEventController.handle);
+
+eventRoutes.delete('/:id', isAuthenticate, deleteEventController.handle);
+
+export { eventRoutes };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { addressRoutes } from './address.routes';
+import { eventRoutes } from './event.routes';
 import { ongRoutes } from './ong.routes';
 import { userRoutes } from './user.routes';
 
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/user', userRoutes);
 router.use('/address', addressRoutes);
 router.use('/ong', ongRoutes);
+router.use('/event', eventRoutes);
 
 export { router };
