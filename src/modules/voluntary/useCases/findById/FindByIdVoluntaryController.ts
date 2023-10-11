a38dbd502e08b32268bd8a5abe3caa1cf29b4f75
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-10-10 23:51:09 -0300

    feat: add find by id voluntary

--- src/modules/voluntary/useCases/findById/FindByIdVoluntaryController.ts
+++ src/modules/voluntary/useCases/findById/FindByIdVoluntaryController.ts
@@ -6,12 +6,13 @@ import { FindByIdVoluntaryUseCase } from './FindByIdVoluntaryUseCase';
 export class FindByIdVoluntaryController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
+    const userId = request.user.id;
 
     const findByIdVoluntaryUseCase = container.resolve(
       FindByIdVoluntaryUseCase,
     );
 
-    const voluntary = await findByIdVoluntaryUseCase.execute(id);
+    const voluntary = await findByIdVoluntaryUseCase.execute({ id, userId });
 
     return response.status(201).json(voluntary);
   }
--- src/modules/voluntary/useCases/findById/FindByIdVoluntaryUseCase.ts
+++ src/modules/voluntary/useCases/findById/FindByIdVoluntaryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { IFindByIdVoluntaryDTO } from '@modules/voluntary/dtos/IFindByIdVoluntaryDTO';
 import { IVoluntaryRepository } from '@modules/voluntary/repositories/IVoluntaryRepository';
 import { Voluntary } from '@prisma/client';
 import { AppError } from '@shared/infra/errors/AppError';
@@ -11,8 +12,8 @@ export class FindByIdVoluntaryUseCase {
     private readonly voluntaryRepository: IVoluntaryRepository,
   ) {}
 
-  async execute(id: string): Promise<Voluntary> {
-    const voluntary = await this.voluntaryRepository.findById(id);
+  async execute({ id, userId }: IFindByIdVoluntaryDTO): Promise<Voluntary> {
+    const voluntary = await this.voluntaryRepository.findById({ id, userId });
 
     if (!voluntary) {
       throw new AppError('Volunteer not found');
